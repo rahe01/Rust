dda7557117315fab56984238f12b8ae584cb62d8
rahe01 <rahatrahe01@gmail.com>
2025-10-16 22:14:55 +0600

    f

--- first.rs
+++
@@ -1,6 +0,0 @@
-fn main(){
-    let a = 5;
-    let b = 10;
-
-    println!("a + b = {}", a + b);
-}
---
+++ main.rs
@@ -0,0 +1,4 @@
+fn main(){
+    println!("Hello, world!");
+    println!("This is a simple Rust program.");
+}
