45374bf0d7f90f63f1a49be02d1cc73e00b9349b
rahe01 <rahatrahe01@gmail.com>
2025-10-23 12:36:38 +0600

    f

--- 3.Common Programming Concepts/hello_cargo/src/main.rs
+++ 3.Common Programming Concepts/hello_cargo/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
 
     println!("Hello world");
+
+    let a = 67;
+
+    if a > 50 {
+        println!("The value is greater than 50");
+    } else {
+        println!("The value is 50 or less");
+    }
     
 }
